e07f059432a2f430aaca2a135a5ab8b84787eaf8
Peter M <sirchappelletto@rambler.ru>
2023-06-18 01:40:47 +0400

    add comment

--- app.rb
+++ app.rb
@@ -10,7 +10,9 @@ def init_db
 	@db.results_as_hash = true
 end
 
+#загружается каждый раз при перезагрузке страницы
 before do
+	#initialize db
 	init_db
 end
 
@@ -28,11 +30,12 @@ get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
-
+#обработчик гет запроса (браузер получает страницу с сервера)
 get '/new' do
   erb :new
 end
 
+#обработчик пост запроса (браузер отправляет данные на сервер)
 post '/new' do
   content = params[:content]
 
